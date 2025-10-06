580de3ec58d3f12698dfc1f8ce75ee412753fa5f
Ali <hisseiny@outlook.fr>
2025-10-06 12:41:37 +0000

    modul3exo

---
+++ main3.py
@@ -0,0 +1,29 @@
+print("------------------ MODULE 3 -------------------------------- \n")
+print("------------------ Exercice 1 -------------------------------- \n")
+# TODO: Create a string variable with a sentence
+phrase = "Je fais mon demenagement today"
+print(phrase)
+# TODO: Convert the string to uppercase
+print("Masjucule ->", phrase.upper())
+# TODO: Convert the string to lowercase
+print("Minuscule ->", phrase.lower())
+# TODO: Capitalize the first letter of each word
+print("Capitalize ->", phrase.title())
+# TODO: Replace a word in the string
+new_phrase = phrase.replace("today", "aujourd'hui")
+print("Avant : ", phrase)
+print("Apres :", new_phrase)
+# TODO: Split the string into a list of words
+split_phrase = phrase.split()
+print(split_phrase)
+
+# TODO: Join the list of words back into a string using a different separator
+
+# TODO: Find the position of a specific word in the string
+
+# TODO: Check if the string starts with a specific word
+
+# TODO: Remove leading and trailing whitespace from a string
+
+# Print the results of each operation
+print(" \n------------------ End 1 -------------------------------- \n")
