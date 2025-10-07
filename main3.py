53c0ab03d7c51706139ff5ae40c2920025978f7f
ahissein <ahissein@eugeniaschool.com>
2025-10-07 14:32:38 +0000

    module sans les 3 dernier TODO

--- main3.py
+++ main3.py
@@ -1,3 +1,4 @@
+import random
 print("------------------ MODULE 3 -------------------------------- \n")
 print("------------------ Exercice 1 -------------------------------- \n")
 # TODO: Create a string variable with a sentence
@@ -109,44 +110,87 @@ while i <= 10:
     i = i+1
 # TODO: Create a guessing game using a while loop
 # (generate a random number and let the user guess until correct)
-import random
+
 
 # TODO: Create a guessing game using a while loop
 # (generate a random number and let the user guess until correct)
 
 # Générer un nombre aléatoire entre 1 et 10
 nombre_secret = random.randint(1, 10)
-
 # Initialiser la variable de l'utilisateur
 guess = None
-
-print("🎲 Bienvenue dans le jeu de devinette !")
+print(" Bienvenue dans le jeu de devinette !")
 print("Devine le nombre (entre 1 et 10) :")
 
 # Tant que la réponse est incorrecte
 while guess != nombre_secret:
     # Demander à l'utilisateur de deviner
-    guess = int(input("👉 Entre ton nombre : "))
+    guess = int(input("Entre ton nombre : "))
 
     # Vérifier la réponse
     if guess < nombre_secret:
-        print("Trop petit 😅, essaie encore !")
+        print("Trop petit , essaie encore !")
     elif guess > nombre_secret:
-        print("Trop grand 😮, réessaie !")
+        print("Trop grand , réessaie !")
     else:
-        print("🎉 Bravo ! Tu as trouvé le nombre :", nombre_secret)
+        print(" Bravo ! Tu as trouvé le nombre :", nombre_secret)
 
 # TODO: Use a while loop to calculate the factorial of a number
 n = 5
 fact = 1
 i = 1
-
 while i <= n:
     fact = fact * i
     i += 1
-
 print("Le factoriel de", n, "est :", fact)
 
 # TODO: Implement a simple calculator using a while loop
 # (continue calculating until the user chooses to exit)
+# TODO: Implement a simple calculator using a while loop
+# (continue calculating until the user chooses to exit)
+
+while True:
+    op = input("Choisis une opération (+, -, *, /) ou 0 pour quitter : ")
+
+    if op == "0":
+        print(" Fin du programme bye.")
+        break  # on sort de la boucle
+
+    n1 = float(input("Donne le 1er nombre : "))
+    n2 = float(input("Donne le 2eme nombre : "))
+
+    if op == "+":
+        print("Résultat pour addition:", n1 + n2)
+    elif op == "-":
+        print("Résultat pour soustraction :", n1 - n2)
+    elif op == "*":
+        print("Résultat pour multiplication :", n1 * n2)
+    elif op == "/":
+        if n2 != 0:
+            print("Résultat pour division :", n1 / n2)
+        else:
+            print(" Erreur : division par zéro impossible.")
+    else:
+        print(" Opération invalide, réessaie.")
+
+# TODO: Create a function that counts the occurrence of each vowel in a string
+def count_vowel(mot):
+    vowel = "aeiuo"
+    cpt = {"a": 0, "e": 0, "i": 0, "u": 0, "o": 0}
+    mot = mot.lower()
+    for char in mot:
+        if char in vowel:
+            cpt[char] += 1
+            
+    print(cpt)  
+mot = input("donne un mot : ")
+count_vowel(mot)
+# TODO: Implement a simple Caesar cipher (shift each letter by a fixed amount)
+
+# TODO: Create a function that generates the NATO phonetic alphabet representation of a word
+
+# TODO: Implement a function that checks if a string is a palindrome
+
+
+# Test each function with sample inputs and print the results
 
